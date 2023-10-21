e5b63ea76d544f9e44f63b9766a132f79f15cc3e
A-Ghoujal <abdogohjal@gmail.com>
2023-10-21 20:58:34 +0100

    inital commit

---
+++ future/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className='welcoming'>Welcome to <br></br>BOOST<br></br> my busniess agency</h1>
+      </header>
+      
+      <section className="App-section">Section 1</section>
+      <section className="App-section">Section 2</section>
+      <section className="App-section">Section 3</section>
+      <section className="App-section">Section 4</section>
+      <section className="App-section">Section 5</section>
+      
+      <footer className="App-footer">Footer</footer>
+    </div>
+  );
+}
+
+export default App;
