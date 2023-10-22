1b9d1be43926ee53c890e37cf57920adca25a612
A-Ghoujal <abdogohjal@gmail.com>
2023-10-22 09:05:02 +0100

    added titles

--- future/src/App.js
+++ future/src/App.js
@@ -5,14 +5,19 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className='welcoming'>Welcome to <br></br>BOOST<br></br> my busniess agency</h1>
+        <h1 className='welcoming'>Welcome to <br></br>BOOST My Business Agency</h1>
+        <h2 className='descreption'>We will take care of your online brand from A to Z.</h2>
       </header>
-      
-      <section className="App-section">Section 1</section>
-      <section className="App-section">Section 2</section>
-      <section className="App-section">Section 3</section>
-      <section className="App-section">Section 4</section>
-      <section className="App-section">Section 5</section>
+
+      <section className="App-section">Create Your Online Logo Brand</section>
+
+      <section className="App-section">Create Your Own Website</section>
+
+      <section className="App-section">Create Your Social Media</section>
+
+      <section className="App-section">Create Your Google Business Account</section>
+
+      <section className="App-section">Online Advertising</section>
       
       <footer className="App-footer">Footer</footer>
     </div>
