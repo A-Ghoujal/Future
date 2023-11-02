0d4b1f2a456381a30a9079227921f275482cea2e
A-Ghoujal <abdogohjal@gmail.com>
2023-11-02 21:27:48 +0100

    added animation for hero section and arrows

--- future/src/App.js
+++ future/src/App.js
@@ -11,34 +11,36 @@ function App() {
   
   return (
     <div className='_carousel'>
-    <div className="App">
-      <header className="App-header">
-      <div className="logo" ></div>
-      
-      <svg id="welcome-svg"  preserveAspectRatio="xMidYMid meet">
-        <text x="50%" y="50%" text-anchor="middle">
-        Welcome to BOOST My Business Agency
-        </text>
-      </svg>
-        <h2 className='description'>We will take care of your online brand from A to Z.</h2>
-      </header>
-
-      <section className="App-section">Create Your Online Logo Brand</section>
-
-      <section className="App-section">Create Your Own Website</section>
-
-      <section className="App-section">Create Your Social Media</section>
-
-      <section className="App-section">Create Your Google Business Account</section>
-
-      <section className="App-section">Online Advertising</section>
-      
-      <footer className="App-footer">Footer </footer>
+        <header className="App-header">
+          <div transition-style="in:custom:swoopy">
+            <div className="logo"></div>
+            <svg id="welcome-svg" preserveAspectRatio="xMidYMid meet">
+              <text x="50%" y="50%" text-anchor="middle">
+                Welcome to BOOST My Business Agency
+              </text>
+              </svg>
+              <div className='arrows'>
+              <div className="indicator">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
     </div>
     </div>
+          </div>
+        </header> 
+        <section className="App-section">We Will Go Through 5 Steps</section>
+        <section className="App-section">Create Your Online Logo Brand</section>
+        <section className="App-section">Create Your Own Website</section>
+        <section className="App-section">Create Your Social Media</section>
+        <section className="App-section">Create Your Google Business Account</section>
+        <section className="App-section">Online Advertising</section>
+        <footer className="App-footer">Footer </footer>
+      </div>
+   
   );
 }
-
 
 export default App;
 
